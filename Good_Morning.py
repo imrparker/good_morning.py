1ea84ffc86bba38d9919fd82a0c3fe67a1bb0ffb
imrparker <30357117+imrparker@users.noreply.github.com>
2017-12-01 16:02:41 -0600

    Add files via upload

---
+++ Good_Morning.py
@@ -0,0 +1,31 @@
+"""A greeting for you in the morning"""
+__author__ = "Parker Ostertag"
+_date__ = "1 December 2017"
+
+import urllib.request
+import urllib.parse
+import re
+from gtts import gTTS
+from pygame import mixer
+
+def main():
+    url = 'https://www.willyweather.com/mn/clay-county/moorhead.html'
+    values = {}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+
+    paragraphs = re.findall(r'<p>(.*?)</p>', str(respData))
+    weather = (paragraphs[0])
+
+    greeting = ("Good morning Parker. The weather today will be" + weather)
+    tts = gTTS(text = greeting, lang = 'en')
+    tts.save("D:/python/test.mp3")
+
+    mixer.init()
+    mixer.music.load('D:/python/test.mp3')
+    mixer.music.play()
+
+main()
